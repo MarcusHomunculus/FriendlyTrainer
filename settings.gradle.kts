3e4c96d0f60052ffc2e08d4773f656de88f0beba
Marcus Homunculus <marcus.homunculus.work@posteo.de>
2022-05-13 22:23:02 +0200

    Initial commit with generated sources

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Friendly_Trainer"
+include(":androidApp")
+include(":shared")
---
+++ shared/src/commonMain/kotlin/com/github/friendlytrainer/Platform.kt
@@ -0,0 +1,5 @@
+package com.github.friendlytrainer
+
+expect class Platform() {
+    val platform: String
+}
