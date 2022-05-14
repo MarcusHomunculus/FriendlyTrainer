ea7794c767a212a2bbf4f599d94293ff7ebaf80b
Marcus Homunculus <marcus.homunculus.work@posteo.de>
2022-05-14 11:57:35 +0200

    Implemented logic for datebase access class

--- shared/src/commonMain/kotlin/com/github/friendlytrainer/storage/Database.kt
+++ shared/src/commonMain/kotlin/com/github/friendlytrainer/storage/Database.kt
@@ -1,6 +1,7 @@
 package com.github.friendlytrainer.storage
 
 import com.github.friendlytrainer.storage.AppStorage
+import com.github.friendlytrainer.ExerciseRecord
 
 internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
     private val database = AppStorage(databaseDriverFactory.createDriver())
@@ -12,7 +13,21 @@ internal class Database(databaseDriverFactory: DatabaseDriverFactory) {
         }
     }
 
-    internal fun allExercises(): List<> {
-        dbQuery
+    internal fun allExercises(): List<ExerciseRecord> {
+        return dbQuery.fullHistory(::exerciseFactory).executeAsList()
+    }
+
+    internal fun lastExercise(): ExerciseRecord {
+        return dbQuery.lastEntryHistory(::exerciseFactory).executeAsList().first()
+    }
+
+    internal fun addExercise(new: ExerciseRecord) {
+        dbQuery.transaction {
+            dbQuery.addExercise(new.date, new.exercise, new.count.toLong())
+        }
+    }
+
+    private fun exerciseFactory(date: String, exercise: String, count: Long): ExerciseRecord {
+        return ExerciseRecord(date, exercise, count.toUInt())
     }
 }
