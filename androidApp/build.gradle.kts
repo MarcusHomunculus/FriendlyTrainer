0ae51370e52f844b105240ec1411522e8a20396d
Marcus Homunculus <marcus.homunculus.work@posteo.de>
2022-05-16 18:05:08 +0200

    Switched to androidplot due to issues with the previous lib

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -22,8 +22,7 @@ android {
 dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("com.jjoe64:graphview:4.2.2")
+    implementation("com.androidplot:androidplot-core:1.5.9")
 }
