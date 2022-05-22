4a2fa5a87991fdb184914737a82d96fe67a41488
Marcus Homunculus <marcus.homunculus.work@posteo.de>
2022-05-22 19:01:24 +0200

    Incorparated the fragments into the app

---
+++ androidApp/src/main/java/com/github/friendlytrainer/android/fragments/AmendFragment.kt
@@ -0,0 +1,6 @@
+package com.github.friendlytrainer.android.fragments
+
+import com.github.friendlytrainer.android.R
+
+class AmendFragment : ExpandableFragment(R.layout.amend_fragment, R.id.amend_hideable) {
+}
---
+++ androidApp/src/main/java/com/github/friendlytrainer/android/fragments/ProgressFragment.kt
@@ -0,0 +1,6 @@
+package com.github.friendlytrainer.android.fragments
+
+import com.github.friendlytrainer.android.R
+
+class ProgressFragment : ExpandableFragment(R.layout.progress_fragment, R.id.progress_hideable) {
+}
