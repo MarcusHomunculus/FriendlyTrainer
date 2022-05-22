a8f95a2a347940fc1147157da6756cf392af4062
Marcus Homunculus <marcus.homunculus.work@posteo.de>
2022-05-22 19:00:37 +0200

    Added parent fragment class

--- androidApp/src/main/java/com/github/friendlytrainer/android/fragments/ExpandableFragment.kt
+++ androidApp/src/main/java/com/github/friendlytrainer/android/fragments/ExpandableFragment.kt
@@ -1,32 +1,28 @@
 package com.github.friendlytrainer.android.fragments
 
-import android.content.Context
 import android.view.View
-import android.widget.ImageView
+import android.widget.Button
 import androidx.annotation.LayoutRes
 import androidx.constraintlayout.widget.Group
 import androidx.fragment.app.Fragment
 import com.github.friendlytrainer.android.R
 
-class ExpandableFragment(
+open class ExpandableFragment(
     @LayoutRes contentLayoutId: Int,
-    private val groupId: Int,
-    private val arrowId: Int
+    private val groupId: Int
 ) : Fragment(contentLayoutId) {
 
     class FragmentIdentifierNotValid(message: String) : Exception(message)
 
-    fun flipState() {
+    fun flipState(buttonId: Int) {
         val group = view?.findViewById<Group>(groupId)
             ?: throw FragmentIdentifierNotValid("The group ID for the fragment ${this::class} is not valid")
-        val arrow = view?.findViewById<ImageView>(arrowId)
-            ?: throw FragmentIdentifierNotValid("The ID for the arrow of fragment ${this::class} is not correct")
         val (visibility, image) = if (group.visibility == View.VISIBLE) {
             View.GONE to R.drawable.expand_arrow
         } else {
             View.VISIBLE to R.drawable.collapse_arrow
         }
         group.visibility = visibility
-        arrow.setImageResource(image)
+        view?.findViewById<Button>(buttonId)?.setCompoundDrawablesWithIntrinsicBounds(0, 0, image, 0)
     }
 }
