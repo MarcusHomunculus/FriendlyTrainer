ffef3ca90ddd94672dddb1cef0c0e0f2034bb96c
Marcus Homunculus <marcus.homunculus.work@posteo.de>
2022-05-24 19:38:07 +0200

    Buttons and wired up the callbacks

--- androidApp/src/main/java/com/github/friendlytrainer/android/viewmodels/MainViewModel.kt
+++ androidApp/src/main/java/com/github/friendlytrainer/android/viewmodels/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.github.friendlytrainer.android.viewmodels
 
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModel
 import com.github.friendlytrainer.android.R
@@ -8,8 +9,12 @@ class MainViewModel : ViewModel() {
 
     enum class InfoView { AMEND, PROGRESS }
 
-    private var _active: InfoView = InfoView.PROGRESS
+    private var _active: InfoView = InfoView.AMEND
 
+    fun focus(which: InfoView) {
+        Log.i("MainViewModel", "Focus update requested")
+        _active = which;
+    }
 
     fun amendVisibility(): Int {
         return if (hasFocus(InfoView.AMEND)) View.VISIBLE else View.GONE
@@ -30,5 +35,4 @@ class MainViewModel : ViewModel() {
     private fun hasFocus(who: InfoView): Boolean {
         return _active == who
     }
-
 }
