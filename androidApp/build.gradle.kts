34c76692304b647d21d53d9e4d705a8505b380b4
Marcus Homunculus <marcus.homunculus.work@posteo.de>
2022-05-30 10:37:03 +0200

    Bumped up version of constrained layout dependency

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -26,7 +26,7 @@ android {
 dependencies {
     implementation(project(":shared"))
     implementation("com.google.android.material:material:1.6.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.3")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.fragment:fragment-ktx:1.4.1")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.androidplot:androidplot-core:1.5.9")
