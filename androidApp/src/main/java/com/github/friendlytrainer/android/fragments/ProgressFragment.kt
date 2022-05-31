ea48ecd5252a24ffeca7ea6bf312d7ddbcb2b128
Marcus Homunculus <marcus.homunculus.work@posteo.de>
2022-05-31 12:50:59 +0200

    Removed obsolete import

--- androidApp/src/main/java/com/github/friendlytrainer/android/fragments/ProgressFragment.kt
+++ androidApp/src/main/java/com/github/friendlytrainer/android/fragments/ProgressFragment.kt
@@ -8,7 +8,6 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
 import com.androidplot.ui.HorizontalPositioning
 import com.androidplot.ui.VerticalPositioning
 import com.androidplot.util.PixelUtils
